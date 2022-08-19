aa509cbd79bf04a0f3a65743f26da783414ce227
Marco Aurelio Pavini Mondini <marco.mondini@venturus.org.br>
2022-08-19 16:41:21 -0300

    start dice rolling

--- app/src/main/java/com/marcoapm/diceroller/MainActivity.kt
+++ app/src/main/java/com/marcoapm/diceroller/MainActivity.kt
@@ -2,10 +2,28 @@ package com.marcoapm.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById<Button>(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            rollDice()
+        }
     }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById<TextView>(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+        resultText.text = "$randomInt"
+    }
+
+
 }
